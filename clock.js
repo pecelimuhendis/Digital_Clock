920fe1a401a983edd8fd5c5373e1adaa0823cdbb
pecelimuhendis <115487510+pecelimuhendis@users.noreply.github.com>
2023-12-16 00:42:19 +0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,43 @@
+/* tanımlamalar-id */
+let hr = document.querySelector('#hr');
+let mn = document.querySelector('#mn');
+let sc = document.querySelector('#sc');
+
+setInterval(() => {
+    /* güncel tarih cekme */
+    let day = new Date();
+    /* çarpma işlemleri 360 derece algılayan kodu saat cinsine çevirmek */
+    let hh = day.getHours() * 30; /* 12 saat*30=360 derece */
+    let mm = day.getMinutes() * 6; /* 60 dakika *6=360derece */
+    let ss = day.getSeconds() * 6;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+
+    /* dijital saat tanımlamalar */
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    let am = h > 12 ? 'PM' : 'AM';
+    /* 24 saat 12 saat */
+    if (h > 12) {
+        h = h - 12;
+    }
+
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+
+})
